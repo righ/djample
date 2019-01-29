0098617939f1e23ecbb0a3bb3b3d33e65c8093d3
crohaco <crohaco@beproud.jp>
2019-01-29 17:56:55 +0900

    addedd sqlite setting

---
+++ apps/settings/sqlite.py
@@ -0,0 +1,32 @@
+from .base import *  # NOQA
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    'SHOW_TOOLBAR_CALLBACK': 'settings.debug_toolbar.allow_all',
+}
+
+
+STATICFILES_DIRS += [
+    ('assets', os.path.join(PROJ_DIR, 'frontend', 'assets')),
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://cache:6379/0',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        },
+    },
+}
