e34ae5dff3a16de5e7a59156ad1c6fe1284f5816
crohaco <crohaco@beproud.jp>
2019-03-13 18:41:48 +0900

    もんだいついき

--- apps/tasks/serializers.py
+++ apps/tasks/serializers.py
@@ -2,6 +2,7 @@
 from rest_framework import serializers
 
 from accounts.models import User
+from accounts.serializers import UserSerializer
 from .models import Status, Tag, Task, Time
 
 
@@ -54,39 +55,48 @@ class TimeListSerializer(serializers.ListSerializer):
 
 class TaskSerializer(serializers.ModelSerializer):
     # times = TimeSerializer(many=True)
-    tags = serializers.PrimaryKeyRelatedField(
+    tag_ids = serializers.PrimaryKeyRelatedField(
         queryset=Tag.objects.filter(),
         many=True, required=False,
     )
-    #owner = serializers.PrimaryKeyRelatedField(
-    #    queryset=User.objects.filter(),
-    #)
 
-    #tag_ids = serializers.PrimaryKeyRelatedField(
-    #    queryset=Tag.objects.filter(),
-    #    many=True, required=False,
-    #    source='tags'
-    #)
-    #owner_id = serializers.PrimaryKeyRelatedField(
-    #    queryset=User.objects.filter(),
-    #    source='owner'
-    #)
+    tags = TagSerializer(many=True, required=False)
+    tag_ids = serializers.PrimaryKeyRelatedField(
+        queryset=Tag.objects.filter(),
+        many=True, required=False,
+        source='tags'
+    )
+    owner = UserSerializer(required=False)
+    owner_id = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.filter(),
+        source='owner'
+    )
+    status_id = serializers.PrimaryKeyRelatedField(
+        queryset=Status.objects.filter(),
+        source='status'
+    )
+    status = StatusSerializer(required=False)
 
     class Meta:
         model = Task
         fields = (
-            'id', 'status', 'content', 
+            'id', 'content', 
             'tags', 
-            #'tag_ids',
+            'tag_ids',
             'owner', 
-            #'owner_id',
+            'owner_id',
+            'status',
+            'status_id',
             'created_at', 'updated_at')
         extra_kwargs = {
             'created_at': {'read_only': True},
             'updated_at': {'read_only': True},
             'times': {'write_only': True},
-            #'tags': {'read_only': True},
-            #'owner': {'read_only': True},
+            'tags': {'read_only': True},
+            'tag_ids': {'write_only': True},
+            'owner': {'read_only': True},
+            'status': {'read_only': True},
+            'status_id': {'write_only': True},
         }
 
 
